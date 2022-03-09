456bfd7abbbc8d45ae4c4710fb117c78e88dd50b
perkinstangka <perkinstangk5@gmail.com>
2022-03-09 19:52:41 +0800

    tampilan form

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-/* eslint-disable eol-last */
+/* eslint-disable prettier/prettier */
 /**
  * @format
  */
 
 import {AppRegistry} from 'react-native';
-import App from './App';
+// import ContohForm from './src/ContohForm';
+import App from './src/App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
---
+++ src/App.js
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+import React from 'react';
+import { View } from 'react-native';
+import ContohForm from './ContohForm';
+
+const App = () => {
+
+
+    return (
+      <View>
+        <ContohForm />
+      </View>
+    );
+  };
+
+export default App;
