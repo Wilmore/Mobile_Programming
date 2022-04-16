efffaf381c2301df95bf833224b483156c86dbed
wilmore <italilpessywilmore@gmail.com>
2022-04-16 22:05:36 +0800

    new

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-/* eslint-disable prettier/prettier */
 /**
  * @format
  */
 
 import {AppRegistry} from 'react-native';
-// import ContohForm from './src/ContohForm';
-import App from './src/App';
+import App from './App';
 import {name as appName} from './app.json';
 
 AppRegistry.registerComponent(appName, () => App);
